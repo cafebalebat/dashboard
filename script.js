990b8c30219bab9150d0cfa8a561aa166f0f653e
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2024-11-11 22:38:14 +0700

    make sidebar navigation (can close and open when click)

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const body = document.querySelector("body"),
         modeToggle = document.querySelector(".mode-toggle");
+        sidebar = document.querySelector("nav");
+        sidebarToggle = document.querySelector(".sidebar-toggle");
 
 modeToggle.addEventListener("click", () =>{
     body.classList.toggle("dark");
 });
+
+sidebarToggle.addEventListener("click", () =>{
+    sidebar.classList.toggle("close");
+})
